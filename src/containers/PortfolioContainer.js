1270beec686bd6856b34ce71440ff71ae4a72f6b
Daniel Park <parkdk96@gmail.com>
2019-11-24 22:50:52 -0800

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -9,6 +9,7 @@ class PortfolioContainer extends Component {
         <h2>My Portfolio</h2>
           {
             //render your portfolio stocks here
+            this.props.stocks.map(stock => <Stock stock={stock} removeStock={this.props.removeStock} /> )
           }
       </div>
     );
